f0311d85143f8e66f402e69b584fe37230fc59a1
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-03-21 14:42:11 +0600

    bottom banner done

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import CategoryList from "./_components/CategoryList";
+import Footer from "./_components/Footer";
 import ProductList from "./_components/ProductList";
 import Slider from "./_components/Slider";
 import GlobalApi from "./_utils/GlobalApi";
@@ -12,6 +14,14 @@ export default async function Home() {
       <Slider sliderList={sliderList} />
       <CategoryList categoryList={categoryList} />
       <ProductList productList={productList} />
+      <Image
+        src="/banner.png"
+        width={1000}
+        height={300}
+        alt="banner"
+        className="h-[400px] w-full rounded-lg object-contain"
+      />
+      <Footer />
     </div>
   );
 }
