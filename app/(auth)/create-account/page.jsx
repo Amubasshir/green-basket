4058e36e4d5dc5b69ab9dff19839620a819f8d5a
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-03-23 15:33:40 +0600

    toast and auth design updated

--- app/(auth)/create-account/page.jsx
+++ app/(auth)/create-account/page.jsx
@@ -41,7 +41,7 @@ const CreateAccount = () => {
   };
   return (
     <div className="my-20 flex items-baseline justify-center">
-      <div className="flex flex-col items-center justify-center border border-gray-200 bg-slate-100 p-10">
+      <div className="flex flex-col items-center justify-center border border-gray-100 bg-green-50 p-10 drop-shadow-xl">
         <Image src="/logo.png" height={200} width={200} alt="logo" />
         <h2 className="text-3xl font-bold">Create Account</h2>
         <h2 className="text-gray-500">
